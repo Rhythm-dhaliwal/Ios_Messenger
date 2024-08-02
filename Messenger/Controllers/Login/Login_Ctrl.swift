cc70d0a85a8db16579e78ca155cf195ceec49934
Rhythm-dhaliwal <rhythmdhaliwal93@gmail.com>
2024-08-02 21:15:23 +0530

    Password field added

--- Messenger/Controllers/Login/Login_Ctrl.swift
+++ Messenger/Controllers/Login/Login_Ctrl.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class Login_Ctrl: UIViewController {
+    // Logo func
+    private let imageView : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Logo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    //scroll func
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.clipsToBounds = true
         return scrollView
     }()
     
+    //email input
     private let emailField : UITextField = {
         let field = UITextField()
         field.autocapitalizationType = .none
+        field.autocorrectionType = .no
         field.returnKeyType = .continue
         field.layer.cornerRadius = 12
         field.layer.borderWidth = 1
@@ -25,19 +35,22 @@ class Login_Ctrl: UIViewController {
         return field
     }()
     
+    //password input
+    private let password : UITextField = {
+       let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.placeholder = "Password..."
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.black.cgColor
+        field.returnKeyType = .continue
+        field.isSecureTextEntry = true
+        return field
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let size = view.width/2
-        // Logo Image
-        let imageView = UIImageView(image: UIImage(named: "Logo"))
-        imageView.contentMode = .scaleAspectFit
-        self.view.addSubview(imageView)
-        
-        imageView.frame = CGRect(x: (view.width-size)/2,
-                                 y: 20,
-                                 width: size,
-                                 height: size)
         
         //View controller background
         view.backgroundColor = .white
@@ -51,16 +64,33 @@ class Login_Ctrl: UIViewController {
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
         scrollView.addSubview(emailField)
+        scrollView.addSubview(password)
         scrollView.frame = view.bounds
-        emailField.frame = CGRect(x: 30, y: imageView.bottom+10, width: scrollView.width-60, height: 52)
-        }
+       
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.width/2
+        imageView.frame = CGRect(x: (view.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: scrollView.width-60,
+                                  height: 52)
+        
+        password.frame = CGRect(x: 30,
+                                y: emailField.bottom+10,
+                                width: scrollView.width-60,
+                                height: 52)
+        
+    }
    
     @objc private func didTapRegister(){
         let vc = Register_Ctrl()
 
         navigationController?.pushViewController(vc, animated: true)
     }
-    
-    
-    
 }
