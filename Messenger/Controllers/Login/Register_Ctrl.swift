9ad7aad763a45cb8b15beb58c8a8d2865b7af682
Rhythm-dhaliwal <rhythmdhaliwal93@gmail.com>
2024-08-07 20:59:40 +0530

    Create Account section has been updated added email,username,password inputs

--- Messenger/Controllers/Login/Register_Ctrl.swift
+++ Messenger/Controllers/Login/Register_Ctrl.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class Register_Ctrl: UIViewController {
     
+    
     private let imageView: UIImageView = {
        let imageView = UIImageView(image: UIImage(named: "Logo"))
         imageView.contentMode = .scaleAspectFill
@@ -22,22 +23,102 @@ class Register_Ctrl: UIViewController {
         return background
     }()
     
+    //Email Address
+    private let EmailField : UITextField = {
+        let email = UITextField()
+        email.autocapitalizationType = .none
+        email.autocorrectionType = .no
+        email.layer.cornerRadius = 12
+        email.layer.borderWidth = 0.5
+        email.layer.borderColor = UIColor.black.cgColor
+        let placeholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedString.Key.foregroundColor:UIColor.lightGray])
+        email.attributedPlaceholder = placeholder
+        email.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha:   0.4)
+        email.textColor = .lightGray
+        return email
+    }()
+    
+    private let UsernameField: UITextField = {
+       let username = UITextField()
+        username.placeholder = "Username"
+        username.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        username.textColor = .lightGray
+        let placeholdertext = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor:UIColor.lightGray])
+        username.attributedPlaceholder = placeholdertext
+        username.layer.cornerRadius = 12
+        username.layer.masksToBounds = true
+        username.autocapitalizationType = .none
+        username.autocorrectionType = .no
+        username.layer.borderWidth = 0.5
+        username.layer.borderColor = UIColor.black.cgColor
+        return username
+    }()
+    
+    private let passwordField: UITextField = {
+        let password = UITextField()
+        password.autocorrectionType = .no
+        password.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        password.autocapitalizationType = . none
+        password.layer.cornerRadius = 12
+        password.layer.masksToBounds = true
+        password.layer.borderColor = UIColor.black.cgColor
+        password.textColor = .lightGray
+        password.layer.borderWidth = 0.5
+        let placeholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor:UIColor.lightGray])
+        password.attributedPlaceholder = placeholder
+        return password
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Create Account"
+        
+        EmailField.delegate = self
+        UsernameField.delegate = self
+        passwordField.delegate = self
         //Add SubViews
         
+        view.frame = view.bounds
         view.addSubview(imageView)
         view.addSubview(background)
         view.sendSubviewToBack(background)
-       
+        view.addSubview(EmailField)
+        view.addSubview(UsernameField)
+        view.addSubview(passwordField)
     }
+    
+    
     override func viewDidLayoutSubviews(){
         let size = view.width/2
         imageView.frame = CGRect(x: (view.width)/4, y: 120, width: size, height: size)
-        background.frame = self.view.bounds
+        background.frame = view.bounds
         
+        EmailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: view.width-60,
+                                  height: 52)
         
+        UsernameField.frame = CGRect(x: 30,
+                                     y: EmailField.bottom+10,
+                                     width: view.width-60,
+                                     height: 52)
+        
+        passwordField.frame = CGRect(x: 30, 
+                                     y: UsernameField.bottom+10,
+                                     width: view.width-60,
+                                     height: 52)
         
    }
+}
+extension Register_Ctrl: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == EmailField {
+            UsernameField.becomeFirstResponder()
+        }
+        else if textField == UsernameField{
+            passwordField.becomeFirstResponder()
+        }
+        return true
+    }
 }
